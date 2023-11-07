58af9ae6f90d3f02b6f2455127a43867dd2a2647
JAGerales <jacob.a.gerales@gmail.com>
2023-11-07 12:19:44 -0800

    dgemv-openmp.cpp updated

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -19,6 +19,17 @@ void my_dgemv(int n, double* A, double* x, double* y) {
       int nthreads = omp_get_num_threads();
       int thread_id = omp_get_thread_num();
       printf("my_dgemv(): Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
+      
+      #pragma omp parallel for reduction(+:sum)
+      for (int i = 0; i < n; i++){ // row
+         int rowOffset = i * n; 
+         double sum = 0.0;
+         for (int j = 0; j < n; j++){ // column
+            sum += A[rowOffset + j] * x[j];
+         }
+         y[i] += sum;
+      }
+
       printf("my_dgemv(): For actual timing runs, please comment out these printf() and omp_get_*() statements. \n");
    }
 
@@ -26,5 +37,12 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    // and you will want to comment out the above parallel code block that prints out
    // nthreads and thread_id so as to not taint your timings
 
+   // parallel region = #pragma omp parallel {}
+   // all code inside {} executes in parallel
+   // each of the A[i] * x computations can be done seperately, in parallel
+
+   // reduction clause can make summations on multiple threads more efficient
+   // reduction (op: list)
+   // pragma omp parallel for reduction (+:sum)
 }
 
